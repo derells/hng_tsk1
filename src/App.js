ffce5e8daa5682d6a9e9ccfd45eb929a7d968e27
derells <adenijiadenrele225@gmail.com>
2022-10-31 15:43:40 +0100

    moop

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
     return (
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="" element={<Home />} />
         </Routes>
       </Router>
     );
